f03a5aa413c6efc7867415b94e7e1cc6c6e0f5fa
Jongchin Lin <jclin29@gmail.com>
2015-02-18 17:22:56 -0500

    Edit delimiter in the code

--- word_count/src/main/scala/wordCountApp.scala
+++ word_count/src/main/scala/wordCountApp.scala
@@ -9,7 +9,7 @@ object wordCountApp {
         val sc = new SparkContext(conf) // initialize a SparkContext, pass a SparkConf object to the constructor
         val lines = sc.textFile(filename) // read a text file as a collection of lines
         // val lines = sc.textFile(filename).cache() // pull the data set into a cluster-wide in-memory cache
-        val words = lines.flatMap(line => line.split(" ")) // split each line into words
+        val words = lines.flatMap(line => line.split("[ \t,:.!]")) // split each line into words
         val pairs = words.map(word => (word, 1)) // map each word to a (word, 1) pair
         val counts = pairs.reduceByKey(_ + _) // or pairs.reduceByKey((a, b) => a + b); reduce value by key
         counts.saveAsTextFile("wc_out.txt")
